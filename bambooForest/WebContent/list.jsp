b3493b20a2d4888f75ca8f2adef883f783176f17
lshdainty <45247068+lshdainty@users.noreply.github.com>
2019-01-03 11:15:24 +0900

    2019/01/03 bamboo 초기작업

---
+++ bambooForest/WebContent/list.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	여기는 리스트가 출력될 예정입니다.
+</body>
+</html>
---
+++ bambooForest/src/bambooforest/action/IndexAction.java
@@ -0,0 +1,14 @@
+package bambooforest.action;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class IndexAction implements Action{
+	public String process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{	
+		//DB에서 게시글 목록을 읽어와 request.setAttribute 할 예정
+		return "/list.jsp";
+	}
+}
